0e0688e22166fe0d98d033ba661ae671cdf10251
Darcy Laycock <sutto@sutto.net>
2010-07-18 07:41:14 +0800

    Add setup_load_paths

---
+++ config/setup_load_paths.rb
@@ -0,0 +1,18 @@
+if ENV['MY_RUBY_HOME'] && ENV['MY_RUBY_HOME'].include?('rvm')
+  rvm_path     = File.dirname(File.dirname(ENV['MY_RUBY_HOME']))
+  rvm_lib_path = File.join(rvm_path, 'lib')
+  # Differentiate between new and old passenger versions
+  if File.exist?(File.join(rvm_lib_path, "rvm", "environment.rb"))
+    $LOAD_PATH.unshift rvm_lib_path
+    require 'rvm'
+    RVM.use_from_path! Dir.pwd # Passenger starts in the app root
+  end
+end
+
+# Select the correct item for which you use below.
+# If you're not using bundler, remove it completely.
+
+ENV['BUNDLE_GEMFILE'] = File.expand_path('../Gemfile', File.dirname(__FILE__))
+
+# If we're using a Bundler 1.0 beta
+require 'bundler/setup'
