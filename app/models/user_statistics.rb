ce95b44d0ce5df61c8a81ce9faee7f4eece7438b
Darcy Laycock <sutto@sutto.net>
2010-08-25 00:13:54 +0800

    Revert "Stub out user registrations info"
    This reverts commit 72ced834ffee001a6bb91c49e38787b8b8d39b32.

--- app/models/user_statistics.rb
+++ app/models/user_statistics.rb
@@ -2,13 +2,12 @@ class UserStatistics
 
   def self.signups_per_day(from = Date.today - 6, to = Date.today)
     from, to = from.to_date, to.to_date
-    # TODO: Make this code work
-    # users = User.select("DATE(users.created_at) AS users_date, count(*) AS count_all").group("users_date")
-    # users = users.having(["users_date > ? AND users_date < ?", from - 1, to + 1]).all
-    # users = users.inject({}) { |a, c| a[c.users_date.to_s] = c.count_all.to_i; a }
-    # results = ActiveSupport::OrderedHash.new
+    users = User.select("DATE(users.created_at) AS users_date, count(*) AS count_all").group("users_date")
+    users = users.having(["users_date > ? AND users_date < ?", from - 1, to + 1]).all
+    users = users.inject({}) { |a, c| a[c.users_date] = c.count_all.to_i; a }
+    results = ActiveSupport::OrderedHash.new
     while from <= to
-      results[from] = rand(100) #users[from].to_i
+      results[from] = users[from].to_i
       from += 1
     end
     results
